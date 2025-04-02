8aa2681153b86fa249bb56385cc1b0284d2a020d
tommarghem <t.marghem@gmail.com>
2025-04-02 15:24:05 +0200

    component2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,30 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
+    const pokemon = pokemonList[0];
     return (
+
+
 
         <figure>
 
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Pokemon" />
-            <figcaption>Bulbasaur</figcaption>
+            {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>????</p>}
+
+
+            <figcaption>{pokemon.name}</figcaption>
 
         </figure>
+
+
 
     )
 }
